8bd660dda97c3c2df71da4c8eaf0315054fdedae
Andrew Li <andrewli@Andrews-MacBook-Pro.local>
2025-04-15 15:51:35 -0500

    Keeping track of counts in the S3 table

--- src/main/java/com/github/poc/s3tables/investments/JoinAccountsWithHoldingsAndStocks.java
+++ src/main/java/com/github/poc/s3tables/investments/JoinAccountsWithHoldingsAndStocks.java
@@ -1,6 +1,5 @@
 package com.github.poc.s3tables.investments;
 
-import com.github.poc.s3tables.investments.utils.MemoryUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
@@ -13,9 +12,10 @@ import static org.apache.spark.sql.functions.current_timestamp;
 @Slf4j
 public class JoinAccountsWithHoldingsAndStocks {
     public static void main(String[] args) throws NoSuchTableException {
-        MemoryUtils.checkHeapMemory();
-
         SparkSession spark = startSession("s3Test");
+
+        log.info("S3 Table POC - Initial count of output table: {}",
+                countNumRowsInOutput(spark));
 
         log.info("S3 Table POC - Reads with Left Join Started");
 
@@ -42,5 +42,18 @@ public class JoinAccountsWithHoldingsAndStocks {
                 .append();
 
         log.info("S3 Table POC - Appending twenty million rows complete");
+
+        log.info("S3 Table POC - Final count of output table: {}",
+                countNumRowsInOutput(spark));
+    }
+
+    private static int countNumRowsInOutput(SparkSession spark) {
+        Dataset<Row> datasetOfCount = spark.sql("""
+                SELECT
+                    COUNT(*) AS NUM_ROWS
+                FROM s3tablesbucket.investments.`reports`
+        """);
+        Row rowOfCount = datasetOfCount.collectAsList().get(0);
+        return rowOfCount.getInt(rowOfCount.fieldIndex("NUM_ROWS"));
     }
 }
